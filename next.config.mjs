52fe0a59e321fa6d2c9020c6a4dd002eb31b81a2
Vladislav Banykin <banykinv@gmail.com>
2024-07-24 15:22:00 +0000

    directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    reactStrictMode: true,
     basePath: '/money-control',
-    distDir: 'build',
+    distDir: './build',
 };
 
 export default nextConfig;
